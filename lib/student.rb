80775a4d7e411233c9afceb97464ad2e7780b25f
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-03 20:02:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,6 @@
 class Student  < User
 
+<<<<<<< HEAD
   def initialize
     @knowledge= []
   end
@@ -11,4 +12,10 @@ class Student  < User
   def knowledge
     @knowledge
   end
+=======
+  def initialize(name)
+    @name = name
+    @knowledge= []
+  end
+>>>>>>> 27a70bccd34db1ebb4278252508ab32d88cd0bca
 end
